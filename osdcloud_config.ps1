1d3014ed0f2bd82a315fab59219910f7049bd650
UpwardQHL <quinn.lobdell@upward-technology.com>
2025-06-19 15:27:02 -0700

    Update osdcloud_config.ps1

--- osdcloud_config.ps1
+++ osdcloud_config.ps1
@@ -10,7 +10,7 @@ New-OSDCloudWorkspace -WorkspacePath C:\OSDCloud
  
 New-OSDCloudUSB
  
-Edit-OSDCloudwinPE -workspacepath C:\OSDCloud -CloudDriver * -WebPSScript https://gist.githubusercontent.com/Jeffhunter88/ed338a1c3aab4ca6abd2dd68a329d53c/raw/osdcloud_config.ps1 -Verbose
+Edit-OSDCloudwinPE -workspacepath C:\OSDCloud -CloudDriver * -WebPSScript https://raw.githubusercontent.com/UpwardQHL/Upward-OSD-Cloud/refs/heads/main/osdcloud_config.ps1 -Verbose
  
 New-OSDCloudISO
  
