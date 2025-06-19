a596afd22bf9261ca469047816f041956f671899
UpwardQHL <quinn.lobdell@upward-technology.com>
2025-06-19 16:31:19 -0700

    Update osdcloud_config.ps1

--- osdcloud_config.ps1
+++ osdcloud_config.ps1
@@ -1,8 +1,10 @@
 Write-Host -ForegroundColor Green "Starting Upward OSDCloud ZTI - QHL"
 Start-Sleep -Seconds 5
 
-Start-OSDCloud -OSVersion 'Windows 11' -OSEdition Pro -OSLanguage en-us -OSLicense Retail -ZTI
+Start-OSDCloud -OSVersion 'Windows 11' -OSEdition Pro -OSLanguage "en-us" -OSLicense Retail -ZTI -Brand 'Upward - QHL'
 
+#Install DAtto
+(New-Object System.Net.WebClient).DownloadFile("https://zinfandel.rmm.datto.com/download-agent/windows/2d46424b-3d1a-4182-8180-a850f3bc0c23", "$env:TEMP/AgentInstall.exe");start-process "$env:TEMP/AgentInstall.exe"
 #Restart from WinPE
 
 Write-Host -ForegroundColor Green “Restarting in 20 seconds!”
