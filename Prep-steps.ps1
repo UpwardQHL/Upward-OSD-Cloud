3c48bf0fb4760ac38e26809193dc1603ad57a849
UpwardQHL <quinn.lobdell@upward-technology.com>
2025-06-19 16:32:48 -0700

    Update Prep-steps.ps1

--- Prep-steps.ps1
+++ Prep-steps.ps1
@@ -11,7 +11,7 @@ New-OSDCloudWorkspace -WorkspacePath C:\OSDCloud
 
 #Edit-OSDCloudwinPE -CloudDriver * -StartOSDCloud "-OSVersion 'Windows 11' -OSEdition Pro -OSLanguage en-us -OSLicense Retail -ZTI -Restart" -Brand 'Upward - QHL' -StartPSCommand 
 #Edit-OSDCloudwinPE -workspacepath C:\OSDCloud -CloudDriver * -WebPSScript https://raw.githubusercontent.com/UpwardQHL/Upward-OSD-Cloud/refs/heads/main/osdcloud_config.ps1 -Verbose
-Edit-OSDCloudwinPE -StartURL "https://raw.githubusercontent.com/UpwardQHL/Upward-OSD-Cloud/refs/heads/main/osdcloud_config.ps1" -Verbose
+Edit-OSDCloudwinPE -CloudDriver * -StartURL "https://raw.githubusercontent.com/UpwardQHL/Upward-OSD-Cloud/refs/heads/main/osdcloud_config.ps1" -Verbose
 
 New-OSDCloudUSB
 New-OSDCloudISO
