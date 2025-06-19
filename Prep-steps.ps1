3d39b56f49d3ec80f545b6fa8cd71b3c22afd9ee
UpwardQHL <quinn.lobdell@upward-technology.com>
2025-06-19 16:04:09 -0700

    Create Prep-steps.ps1

---
+++ Prep-steps.ps1
@@ -0,0 +1,18 @@
+Set-ExecutionPolicy RemoteSigned -Force
+ 
+Install-Module OSD -Force
+ 
+Import-Module OSD -Force
+ 
+New-OSDCloudTemplate -SetInputLocale en-us
+ 
+New-OSDCloudWorkspace -WorkspacePath C:\OSDCloud
+ 
+New-OSDCloudUSB
+
+ 
+#Edit-OSDCloudwinPE -workspacepath C:\OSDCloud -CloudDriver * -WebPSScript https://raw.githubusercontent.com/UpwardQHL/Upward-OSD-Cloud/refs/heads/main/osdcloud_config.ps1 -Verbose
+ 
+New-OSDCloudISO
+ 
+Update-OSDCloudUSB
