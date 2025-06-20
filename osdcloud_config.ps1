02259994da6e8d0f0c0b6fa5be4a2e6562a6fae7
UpwardQHL <quinn.lobdell@upward-technology.com>
2025-06-19 17:37:21 -0700

    Update osdcloud_config.ps1

--- osdcloud_config.ps1
+++ osdcloud_config.ps1
@@ -11,11 +11,11 @@ powercfg -change -standby-timeout-ac 0
 powercfg -change -monitor-timeout-ac 0
 
 #Install DAtto
-(New-Object System.Net.WebClient).DownloadFile("https://zinfandel.rmm.datto.com/download-agent/windows/2d46424b-3d1a-4182-8180-a850f3bc0c23", "$env:TEMP/AgentInstall.exe");start-process "$env:TEMP/AgentInstall.exe"
+#(New-Object System.Net.WebClient).DownloadFile("https://zinfandel.rmm.datto.com/download-agent/windows/2d46424b-3d1a-4182-8180-a850f3bc0c23", "$env:TEMP/AgentInstall.exe");start-process "$env:TEMP/AgentInstall.exe"
 #Restart from WinPE
 
 Write-Host -ForegroundColor Green “Restarting in 20 seconds!”
 
-Start-Sleep -Seconds 20
+Start-Sleep -Seconds 10
 
 wpeutil reboot
