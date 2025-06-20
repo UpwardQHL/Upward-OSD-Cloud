a177ed9b8168268334ab9ee3f1f1635a21187d68
UpwardQHL <quinn.lobdell@upward-technology.com>
2025-06-19 17:36:37 -0700

    Update osdcloud_config.ps1

--- osdcloud_config.ps1
+++ osdcloud_config.ps1
@@ -1,7 +1,14 @@
 Write-Host -ForegroundColor Green "Starting Upward OSDCloud ZTI - QHL"
 Start-Sleep -Seconds 5
 
-Start-OSDCloud -OSVersion 'Windows 11' -OSEdition Pro -OSLanguage "en-us" -OSLicense Retail -ZTI -Brand 'Upward - QHL'
+Start-OSDCloud -OSVersion 'Windows 11' -OSEdition Pro -OSLanguage "en-us" -OSLicense Retail -ZTI 
+#-Brand 'Upward - QHL'
+
+# 1. Set sleep settings to 'Never' when plugged in
+powercfg -change -standby-timeout-ac 0
+
+# 1b. Set screen timeout to 'Never' when plugged in
+powercfg -change -monitor-timeout-ac 0
 
 #Install DAtto
 (New-Object System.Net.WebClient).DownloadFile("https://zinfandel.rmm.datto.com/download-agent/windows/2d46424b-3d1a-4182-8180-a850f3bc0c23", "$env:TEMP/AgentInstall.exe");start-process "$env:TEMP/AgentInstall.exe"
