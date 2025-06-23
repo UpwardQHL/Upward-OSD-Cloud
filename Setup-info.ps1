de5eaa4575ebd7ecc9ffceb55883e388ab28d93b
UpwardQHL <quinn.lobdell@upward-technology.com>
2025-06-23 15:31:18 -0700

    Update Setup-info.ps1

--- Setup-info.ps1
+++ Setup-info.ps1
@@ -7,11 +7,18 @@ Import-Module OSD -Force
 New-OSDCloudTemplate
  
 New-OSDCloudWorkspace -WorkspacePath C:\OSDCloud
- 
 
-Edit-OSDCloudwinPE -workspacepath C:\OSDCloud -CloudDriver * -StartURL https://raw.githubusercontent.com/UpwardQHL/Upward-OSD-Cloud/refs/heads/main/osdcloud_config.ps1 -Verbose
+#Cleanup Languages
+$KeepTheseDirs = @('boot','efi','en-us','sources','fonts','resources')
+Get-ChildItem "$(Get-OSDCloudWorkspace)\Media" | Where {$_.PSIsContainer} | Where {$_.Name -notin $KeepTheseDirs} | Remove-Item -Recurse -Force
+Get-ChildItem "$(Get-OSDCloudWorkspace)\Media\Boot" | Where {$_.PSIsContainer} | Where {$_.Name -notin $KeepTheseDirs} | Remove-Item -Recurse -Force
+Get-ChildItem "$(Get-OSDCloudWorkspace)\Media\EFI\Microsoft\Boot" | Where {$_.PSIsContainer} | Where {$_.Name -notin $KeepTheseDirs} | Remove-Item -Recurse -Force
+
 New-OSDCloudWorkSpaceSetupCompleteTemplate
+Edit-OSDCloudwinPE -workspacepath C:\OSDCloud -CloudDriver * -StartURL https://raw.githubusercontent.com/UpwardQHL/Upward-OSD-Cloud/refs/heads/main/osdcloud_config.ps1 -Verbose
+
 #Edit the template file, insert the auto attend
+#Maybe do it in the automate folderm maybe edit the confied files
 
 New-OSDCloudISO
 New-OSDCloudUSB
