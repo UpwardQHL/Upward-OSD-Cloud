2be64ffff648ae309c166cdafa8d606d90f6a3ed
UpwardQHL <quinn.lobdell@upward-technology.com>
2025-06-24 11:35:34 -0700

    Update Prep-steps.ps1

--- Prep-steps.ps1
+++ Prep-steps.ps1
@@ -5,9 +5,15 @@ Install-Module OSD -Force
 Import-Module OSD -Force
  
 New-OSDCloudTemplate -SetInputLocale en-us
+
+#Cleanup Languages
+$KeepTheseDirs = @('boot','efi','en-us','sources','fonts','resources')
+Get-ChildItem "$(Get-OSDCloudWorkspace)\Media" | Where {$_.PSIsContainer} | Where {$_.Name -notin $KeepTheseDirs} | Remove-Item -Recurse -Force
+Get-ChildItem "$(Get-OSDCloudWorkspace)\Media\Boot" | Where {$_.PSIsContainer} | Where {$_.Name -notin $KeepTheseDirs} | Remove-Item -Recurse -Force
+Get-ChildItem "$(Get-OSDCloudWorkspace)\Media\EFI\Microsoft\Boot" | Where {$_.PSIsContainer} | Where {$_.Name -notin $KeepTheseDirs} | Remove-Item -Recurse -Force
  
 New-OSDCloudWorkspace -WorkspacePath C:\OSDCloud
- 
+New-OSDCloudWorkSpaceSetupCompleteTemplate 
 
 #Edit-OSDCloudwinPE -CloudDriver * -StartOSDCloud "-OSVersion 'Windows 11' -OSEdition Pro -OSLanguage en-us -OSLicense Retail -ZTI -Restart" -Brand 'Upward - QHL' -StartPSCommand 
 #Edit-OSDCloudwinPE -workspacepath C:\OSDCloud -CloudDriver * -WebPSScript https://raw.githubusercontent.com/UpwardQHL/Upward-OSD-Cloud/refs/heads/main/osdcloud_config.ps1 -Verbose
